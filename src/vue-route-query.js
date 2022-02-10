9c26d679597a7075c877d72a0ae3e14a9a46f520
hjmmc <haojie.liu@outlook.com>
2022-02-10 12:09:08 +0800

    add popo state support

--- src/vue-route-query.js
+++ src/vue-route-query.js
@@ -20,6 +20,9 @@ function setVNodeData(routeValue, binding, vNode) {
   vNode.context.$set(data, lastKey, routeValue)
 }
 
+// 浏览器是否支持 popState
+const isSupportPopState = !!window.history.pushState
+
 /**
  * 路由钩子，监听变量并设置路由参数
  * 默认用法：<div v-rq="query.page" /> 根据传入的值支持自动转换为 string,number,boolean 不支持其它类型，可通过修饰符修改值类型
@@ -28,30 +31,35 @@ function setVNodeData(routeValue, binding, vNode) {
  * 修饰符 number: <div v-rq:page.number="query.page" /> 指定数据类型为 number
  * 修饰符 boolean: <div v-rq:enable.boolean="query.enable" /> 指定数据类型为 boolean
  */
-export default {
+ export default {
   bind: function (el, binding, vNode) {
-    vNode.context.$watch(
-      () => vNode.context.$route.query[binding.arg || binding.expression],
-      (val) => {
-        setVNodeData(val, binding, vNode)
-      }
-    )
+    if (binding.modifiers.popstate) {
+      isSupportPopState && window.addEventListener('popstate', binding.value)
+      return
+    }
+    const defaultRouteValue = binding.value
+    vNode.context.$watch(() => vNode.context.$route.query[binding.arg || binding.expression], (val) => {
+      setVNodeData(val, defaultRouteValue, binding, vNode)
+    })
     const routeValue = vNode.context.$route.query[binding.arg || binding.expression]
-    setVNodeData(routeValue, binding, vNode)
+    setVNodeData(routeValue, defaultRouteValue, binding, vNode)
   },
   update: function (el, binding, vNode) {
+    if (binding.modifiers.popstate) return
     if (binding.oldValue !== binding.value) {
       const route = { ...vNode.context.$route }
       if (route.query[binding.arg || binding.expression] === `${binding.value}`) return
-      route.query = {
-        ...route.query,
-        [binding.arg || binding.expression]: binding.value,
-      }
+      route.query = { ...route.query, [binding.arg || binding.expression]: binding.value }
       if (binding.modifiers.replace) {
-        vNode.context.$router.replace(route).finally(() => {})
+        vNode.context.$router.replace(route).finally(() => { })
       } else {
-        vNode.context.$router.push(route).finally(() => {})
+        vNode.context.$router.push(route).finally(() => { })
       }
     }
   },
-}
+  unbind (el, binding, vNode) {
+    if (binding.modifiers.popstate) {
+      isSupportPopState && window.removeEventListener('popstate', binding.value)
+    }
+  }
+})
